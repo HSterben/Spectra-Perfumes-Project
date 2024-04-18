efc3ce6fec9a699ddd98040e455b05ef117ec3f5
Hadi Sasa <2291195@edu.vaniercollege.qc.ca>
2024-04-18 11:49:24 -0400

    Worked on invoice model

--- app/models/Invoice.php
+++ app/models/Invoice.php
@@ -3,116 +3,77 @@ namespace app\models;
 
 class Invoice{
     public $invoice_id;
-	public $last_name;
-	public $email;
-	public $weekly_flyer;
-	public $mailing_list;
-
-	public function __construct($object = null){
-		if($object == null)
-			return;// avoid this running when there is no parameter....
-		$this->first_name = $object->first_name;
-		$this->last_name = $object->last_name;
-		$this->email = $object->email;
-		$this->weekly_flyer = $object->weekly_flyer;
-		$this->mailing_list = $object->mailing_list;
-	}
+	public $invoice_title;
+	public $project_id;
+	public $business_name;
+	public $address;
+	public $phone_number;
+	public $perfume_price;
+	public $return_quantity;
+	public $perfume_code;
+    public $comment;
 
 
-	/* insert the record in the data file */
+    //create
 	public function insert(){
-		$filename = 'resources/People.txt';
-		//open a file for writing (append)
-		$file_handle = fopen($filename, 'a'); //a is for append, w for writing from the start
-		//obtain exclusive access to the file to avoid data corruption
-		flock($file_handle, LOCK_EX);
-		//format the data and write to the file
-		$data = json_encode($this);
-		fwrite($file_handle, $data . "\n");//place a single record on each line
-		//release the exclusive access to the file
-		flock($file_handle, LOCK_UN);
-		//close the file
-		fclose($file_handle);
+		$SQL = 'INSERT INTO profile(invoice_id,invoice_title,project_id,business_name,address,phone_number,perfume_price,return_quantity) VALUE (:user_id,:first_name,:last_name)';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['user_id'=>$this->user_id,
+			'first_name'=>$this->first_name,
+			'last_name'=>$this->last_name]
+		);
+	}
+
+	//read
+	public function getForUser($user_id){
+		$SQL = 'SELECT * FROM profile WHERE user_id = :user_id';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['user_id'=>$user_id]
+		);
+		//there is a mistake in the next line
+		$STMT->setFetchMode(PDO::FETCH_CLASS,'app\models\profile');//set the type of data returned by fetches
+		return $STMT->fetch();//return (what should be) the only record
+	}
+
+	public function getAll(){
+		$SQL = 'SELECT * FROM profile';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute();
+		$STMT->setFetchMode(PDO::FETCH_CLASS,'app\models\Profile');//set the type of data returned by fetches
+		return $STMT->fetchAll();//return all records
+	}
+
+	public function getByName($name){//search
+		$SQL = 'SELECT * FROM profile WHERE CONCAT(first_name,\' \',last_name) = :name';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['name'=>$name]
+		);
+		$STMT->setFetchMode(PDO::FETCH_CLASS,'app\models\Profile');//set the type of data returned by fetches
+		return $STMT->fetchAll();//return all records
 	}
 
 
+	//update
+	//you can't change the user_id that's a business logic choice that gets implemented in the model
 	public function update(){
-		//read the entire file
-		$filename = "resources/People.txt";
-		//get the contents of the file in this array (line by line)
-		$file_contents = file($filename);
-
-		//open the new version of the same file
-		$file_handle = fopen($filename, 'w');
-		//obtain a lock on the file (avoid reading data that is changing)
-		flock($file_handle, LOCK_EX);
-		//start at the first line
-		$counter = 0;
-		$size = count($file_contents);
-		while($counter < $size){
-			if($this->id != $counter){
-				fwrite($file_handle, $file_contents[$counter]);
-			}else{
-				//add the modified record
-				//format the data and write to the file
-				unset($this->id);
-				$data = json_encode($this);
-				fwrite($file_handle, $data . "\n");//place a single record on each line
-			}
-			$counter++;//next record
-		}		
-		flock($file_handle, LOCK_UN);
-		fclose($file_handle);
-
+		$SQL = 'UPDATE profile SET first_name=:first_name,last_name=:last_name WHERE profile_id = :profile_id';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['profile_id'=>$this->profile_id,
+			'first_name'=>$this->first_name,
+			'last_name'=>$this->last_name]
+		);
 	}
 
-	public static function getAll(){
-		//read the file and return the collection of people (all Person records)
-		$filename = 'resources/People.txt';
-		$records = file($filename);
-		//TODO: process the JSON strings into objects
-		foreach ($records as $key => $value) {
-			//can I typecase objects in PHP?
-			$object = json_decode($value);
-			$person = new \app\models\Person($object);
-			$records[$key] = $person;
-		}
-		return $records;
-	}
-
-	//delete a record at line $id in the file
-	public static function delete($id){
-		//read the entire file
-		$filename = "resources/People.txt";
-		//get the contents of the file in this array (line by line)
-		$file_contents = file($filename);
-
-		//open the new version of the same file
-		$file_handle = fopen($filename, 'w');
-		//obtain a lock on the file (avoid reading data that is changing)
-		flock($file_handle, LOCK_EX);
-		//start at the first line
-		$counter = 0;
-		$size = count($file_contents);
-		while($counter < $size){
-			if($id != $counter){
-				fwrite($file_handle, $file_contents[$counter]);
-			}//else it is skipped
-			$counter++;//next record
-		}		
-		flock($file_handle, LOCK_UN);
-		fclose($file_handle);
-	}
-
-	public static function get($id){
-		//read the file and return the collection of people (all Person records)
-		$filename = 'resources/People.txt';
-		$records = file($filename);
-		//process the JSON string into an object
-		$object = json_decode($records[$id]);
-		$person = new \app\models\Person($object);
-		$person->id = $id;
-		//return the record
-		return $person;
+	//delete
+	public function delete(){
+		$SQL = 'DELETE FROM profile WHERE profile_id = :profile_id';
+		$STMT = self::$_conn->prepare($SQL);
+		$STMT->execute(
+			['profile_id'=>$this->profile_id]
+		);
 	}
 }
