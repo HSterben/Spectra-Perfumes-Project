c3a5e3066a22bb7f79121a5aaad05675ebfc098b
Marc-Anthony307 <112972669+Marc-Anthony307@users.noreply.github.com>
2024-04-26 01:37:54 -0400

    Added CSS and alters some views

--- app/views/Invoice/list.php
+++ app/views/Invoice/list.php
@@ -1,31 +1,36 @@
 <?= $this->view('Shared/header'); ?>
+<link rel="stylesheet" href="/app/styles.css">
+<button class=button2> <a href='/Invoice/create'>Add a new invoice</a></button>
 
-<a href='/Invoice/create'>Add a new invoice</a>
-<form method="post" action="/Invoice/confirmBulkDelete">
+<form method="post" action="/Invoice/confirmBulkDelete"  style="display: inline;">
+    <button type="submit">Bulk Delete</button>
+
     <div class='container'>
+        <div class=header>Home</div>
         <table>
-            <button type="submit">Bulk Delete</button>
+
 </form>
 <br><br>
 <form method='post' action=''>
+
     <input type="text" name="query" placeholder="Search invoices...">
-    <button type="submit" name="action">Search</button>
-</form>
+    <!-- <button type="submit" name="action">Search</button> -->
+
+
+</form><br><br>
 <?php foreach ($data as $invoice): ?>
-    <tr>
-        <td><input type="checkbox" name="selected_invoices[]" value="<?= $invoice->invoice_id ?>"></td>
-        <td><?= $invoice->invoice_id ?></td>
-        <td><a href='/Invoice/read/<?= $invoice->invoice_id ?>'>
-                <?= $invoice->invoice_title ?>
-            </a></td>
-        <?php
-        echo "<td><br><a href='/Invoice/update/{$invoice->invoice_id}'class='btn'>Edit</a>
-                <a href='/Invoice/delete/{$invoice->invoice_id}'class='btn'>Delete</a>
-                <a href='/Note/create/{$invoice->invoice_id}'class='btn'>Create Note</a>
-                <a href='/Note/update/{$invoice->invoice_id}'class='btn'>Update Note</a>
-                <a href='/Note/delete/{$invoice->invoice_id}'class='btn'>Delete Note</a></td>";
-        ?>
-    </tr>
+    <div class="invoice-container">
+        <input type="checkbox" name="selected_invoices[]" value="<?= $invoice->invoice_id ?>">
+        <span class="invoice-id"><?= $invoice->invoice_id ?></span>
+        <span class="invoice-title"><a href='/Invoice/read/<?= $invoice->invoice_id ?>' class="invoice-title"><?= $invoice->invoice_title ?></a></span>
+        <div class='invoice-actions'>
+            <a href='/Invoice/update/<?= $invoice->invoice_id ?>' class='btn'>Edit</a>
+            <a href='/Invoice/delete/<?= $invoice->invoice_id ?>' class='btn'>Delete</a>
+            <a href='/Note/create/<?= $invoice->invoice_id ?>' class='btn'>Create Note</a>
+            <a href='/Note/update/<?= $invoice->invoice_id ?>' class='btn'>Update Note</a>
+            <a href='/Note/delete/<?= $invoice->invoice_id ?>' class='btn'>Delete Note</a>
+        </div>
+    </div>
 <?php endforeach; ?>
 </table>
 </div>
